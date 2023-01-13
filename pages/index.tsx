4e9169b5c58ac836d930deb837c9fda9104c8808
ilhomupgrade <122490412+ilhomupgrade@users.noreply.github.com>
2023-01-13 08:21:35 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <Page className="flex flex-col gap-12">
       <section className="flex flex-col gap-6">
-        <Text variant="h1">Porta</Text>
+        <Text variant="h1">Portal</Text>
         <Text className="text-zinc-600">
           Психологический помощник Portal с искусственным интеллектом.
         </Text>
